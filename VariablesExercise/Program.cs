88d0cd061bf86ebeddf67c6fbb7de1238541b5d8
Richard Morris <richmorris55@yahoo.com>
2023-01-30 14:13:52 -0800

    CreatedStory

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            string dinosaurName = "Dino";
+
+            int dinoAge = 25;
+
+            char dinoNickname = 'D';
+
+            bool dinoIsNice = true;
+
+            double dinoSalary = 1200000.73;
+
+            decimal dinoSpeed = 8.3375m;
+
+            string niceMsg = dinoIsNice ? "Hes a nice dinosaur" : "Hes not a nice dinosaur";
+
+
+            Console.WriteLine($"There once was a fast dinosaur named {dinosaurName}. Dino is {dinoAge} years old. Dino's best friends call him {dinoNickname}. {niceMsg} to all his friends. Dino makes {dinoSalary} dollars a year so he shares his money with his friends. Dino is a professional sprinter. Dino can run a 200 meter dash in {dinoSpeed} seconds. I wish I was fast like {dinosaurName}.");
+
+            
+
         }
     }
 }
